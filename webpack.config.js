a0b8215fcae218bebeddff3a516253ce9e46dca3
MuhammedYashar <mteifurov@mail.ru>
2020-12-23 18:50:30 +0300

    main

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -8,9 +13,37 @@ module.exports = {
     filename: 'bundle.[hash].js',
     path: path.resolve(__dirname, 'dist')
   },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@' : path.resolve(__dirname,'src'),
+      '@core' : path.resolve(__dirname,'src/core')
+    }
+  }
+  ,
   plugins: [
-    
-
-
-  ]
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html' 
+    }),
+    new CopyPlugin({ 
+        from: path.resolve(__dirname,'favicon.ico'),
+        to: path.resolve(__dirname,'dist') 
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.[hash].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
 }
